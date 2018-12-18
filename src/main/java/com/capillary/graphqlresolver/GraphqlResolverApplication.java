b56f5ee390992092c13081d95fb6535dc66f4217
Saurabh Sharma <saurabh.sharma@capillarytech.com>
2018-12-18 17:05:44 +0530

    git init

---
+++ src/main/java/com/capillary/graphqlresolver/GraphqlResolverApplication.java
@@ -0,0 +1,14 @@
+package com.capillary.graphqlresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlResolverApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/capillary/graphqlresolver/GraphqlResolverApplicationTests.java
@@ -0,0 +1,17 @@
+package com.capillary.graphqlresolver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphqlResolverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
