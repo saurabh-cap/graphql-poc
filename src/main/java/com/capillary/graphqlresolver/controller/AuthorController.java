a3a931cd6fd81829369628f542b059a04ec42bab
Saurabh Sharma <saurabh.sharma@capillarytech.com>
2019-01-08 09:57:36 +0530

    Adding capability to fire graphql query from server natively

--- src/main/java/com/capillary/graphqlresolver/controller/AuthorController.java
+++ src/main/java/com/capillary/graphqlresolver/controller/AuthorController.java
@@ -4,10 +4,22 @@ import com.capillary.graphqlresolver.models.Author;
 import com.capillary.graphqlresolver.repository.AuthorRepository;
 import com.capillary.graphqlresolver.util.QueryBuilder;
 import com.capillary.graphqlresolver.util.impl.GraphQLQueryBuilder;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 
 @RestController
 @RequestMapping(path = "/author", produces = "application/json")
@@ -19,5 +31,27 @@ public class AuthorController {
     public String findAll() {
         QueryBuilder queryBuilder = new GraphQLQueryBuilder();
         return queryBuilder.build("query", Author.class);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/sample")
+    public String sampleQuery() {
+        final File schemaFile = new File(getClass().getClassLoader().getResource("schema.graphqls").getFile());
+
+        // each registry is merged into the main registry
+        final TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
+        final SchemaParser schemaParser = new SchemaParser();
+        typeRegistry.merge(schemaParser.parse(schemaFile));
+
+        final RuntimeWiring runtimeWiring = newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .build();
+
+        final SchemaGenerator schemaGenerator = new SchemaGenerator();
+        final GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+
+        final GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        final ExecutionResult executionResult = build.execute("{hello}");
+
+        return executionResult.getData().toString();
     }
 }
--- src/main/java/com/capillary/graphqlresolver/repository/AuthorRepository.java
+++ src/main/java/com/capillary/graphqlresolver/repository/AuthorRepository.java
@@ -4,7 +4,6 @@ import com.capillary.graphqlresolver.models.Author;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Repository
