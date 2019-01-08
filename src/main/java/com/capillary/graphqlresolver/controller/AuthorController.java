7a8b0878b0df32ae056506f265017d4f7966d11f
Saurabh Sharma <saurabh.sharma@capillarytech.com>
2019-01-08 12:53:50 +0530

    Using QueryUtil to executeQuery in AuthorController

--- src/main/java/com/capillary/graphqlresolver/controller/AuthorController.java
+++ src/main/java/com/capillary/graphqlresolver/controller/AuthorController.java
@@ -4,22 +4,12 @@ import com.capillary.graphqlresolver.datafetcher.AllAuthorsDataFetcher;
 import com.capillary.graphqlresolver.models.Author;
 import com.capillary.graphqlresolver.repository.AuthorRepository;
 import com.capillary.graphqlresolver.util.QueryBuilder;
+import com.capillary.graphqlresolver.util.QueryUtil;
 import com.capillary.graphqlresolver.util.impl.GraphQLQueryBuilder;
-import graphql.ExecutionResult;
-import graphql.GraphQL;
-import graphql.schema.GraphQLSchema;
-import graphql.schema.idl.RuntimeWiring;
-import graphql.schema.idl.SchemaGenerator;
-import graphql.schema.idl.SchemaParser;
-import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.File;
-
-import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 
 @RestController
 @RequestMapping(path = "/author", produces = "application/json")
@@ -30,32 +20,21 @@ public class AuthorController {
     @Autowired
     AllAuthorsDataFetcher allAuthorsDataFetcher;
 
+    @Autowired
+    private QueryUtil queryUtil;
+
     @RequestMapping(method = RequestMethod.GET)
     public String findAll() {
         QueryBuilder queryBuilder = new GraphQLQueryBuilder();
         return queryBuilder.build("query", Author.class);
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/sample")
+    @RequestMapping(method = RequestMethod.GET, path = "/allAuthors")
     public String sampleQuery() {
-        final File schemaFile = new File(getClass().getClassLoader().getResource("schema.graphqls").getFile());
-
-
-        final RuntimeWiring runtimeWiring = newRuntimeWiring()
-                .type("Query", builder -> builder.dataFetcher("allAuthors", allAuthorsDataFetcher))
-                .build();
-
-        final TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
-        typeRegistry.merge(new SchemaParser().parse(schemaFile));
-        final GraphQLSchema graphQLSchema = new SchemaGenerator().makeExecutableSchema(typeRegistry, runtimeWiring);
-
-        final GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
-        final ExecutionResult executionResult = build.execute("{" +
+        return queryUtil.executeQuery("{" +
                 "  allAuthors{" +
                 "    id" +
                 "  }" +
-                "}");
-
-        return executionResult.getData().toString();
+                "}").getData().toString();
     }
 }
